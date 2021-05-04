cda533a5b61bb633bce8ca6a1464c7745517c85e
Sree Gajula <gajulas@oregonstate.edu>
2021-05-03 18:18:00 -0700

    added list

--- average_list.py
+++ average_list.py
@@ -1,6 +1,8 @@
-def average_list(a, b, c):
-    sum = a + b + c
-    result = sum/3
+def average_list(av_list):
+    sum_items = 0
+    for i in range(len(av_list)):
+        sum_items = sum_items + av_list[i]
+    result = sum_items/len(av_list)
     return(result)
 
 
--- test_average_list.py
+++ test_average_list.py
@@ -3,13 +3,13 @@ import average_list
 
 class testList(unittest.TestCase):
         def test_list(self):
-            result = average_list.average_list(1, 2, 3)
+            result = average_list.average_list(av_list = [1, 2, 3])
             self.assertEqual(result,2)
 
-        def test_list_2(self):
-            result = average_list.average_list(1, 1, 1)
-            self.assertEqual(result,1)
-
         def test_list_fail(self):
-            result = average_list.average_list(0, 1, -2)
+            result = average_list.average_list(av_list = [])
+            self.assertEqual(result,0)
+
+        def test_list_fail_2(self):
+            result = average_list.average_list(av_list = [0, 1, -2])
             self.assertEqual(result,1)
